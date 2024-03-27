143ae9f1cd1069cf019cd6c2916da77ad422bad5
Wolke <98122676+Wolke009@users.noreply.github.com>
2024-03-27 17:08:45 +0000

    Creating the sxhkdrc file

--- installation.sh
+++ installation.sh
@@ -11,3 +11,12 @@ sudo chmod +x /home/$USER/.config/bspwm/bspwmrc
 
 #Copy the bspwm-file to the created direction
 cp -r /home/$USER/bspwm-arch-rice/scripts/bspwmrc /home/$USER/.config/bspwm/
+
+#Create the sxhkd directory
+mkdir -p /home/$USER/.config/sxhkd/
+
+#Copy the sxhkdrc-file to the created direction
+cp -r /home/$USER/bspwm-arch-rice/scripts/sxhkdrc /home/$USER/.config/sxhkd/
+
+#Make the file executeble
+sudo chmod +x /home/$USER/.config/sxhkd/sxhkdrc

f2867d608fa665102e767d03b3a523634de8462e
Wolke <98122676+Wolke009@users.noreply.github.com>
2024-05-25 18:47:44 +0200

    Adding backgrounds

--- installation.sh
+++ installation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Installing the base packages
-sudo xbps-install bspwm sxhkd
+sudo xbps-install bspwm sxhkd feh
 
 #Creating the directions and the bspwm-file
 mkdir -p /home/$USER/.config/bspwm && touch /home/$USER/.config/bspwm/
@@ -20,3 +20,6 @@ cp -r /home/$USER/bspwm-arch-rice/scripts/sxhkdrc /home/$USER/.config/sxhkd/
 
 #Make the file executeble
 sudo chmod +x /home/$USER/.config/sxhkd/sxhkdrc
+
+#!/bin/bash
+echo "exec bspwm" >> ~/.xinitrc
